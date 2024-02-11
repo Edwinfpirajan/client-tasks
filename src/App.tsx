0fa78e3b5c4e70cd70fab0b223824c1df28705cc
Edwinfpirajan <efpa1998@hotmail.com>
2024-02-11 13:29:30 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Test</p>
+    </>
+  );
+}
+
+export default App;
